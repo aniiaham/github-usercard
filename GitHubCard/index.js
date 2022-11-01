87f90b5509e2efe5d11d9cea22ec629e499562df
aniiaham <aniya_axm@icloud.com>
2022-10-31 22:13:40 -0400

    all tasks are done except 5

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,18 @@
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/aniiaham')
+  .then (response => {
+    // console.log(response);
+    const me = aboutMe(response.data)
+    const entryPoint = document.querySelector('.cards')
+    entryPoint.appendChild(me);  
+  }) .catch (err => {
+    // console.error(err); 
+  })
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,7 +37,13 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followersArray = [
+//   'tetondan',
+//   'dustinmyers',
+//   'justsml',
+//   'luishrd',
+//   'bigknell'
+// ];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,7 +64,56 @@ const followersArray = [];
       </div>
     </div>
 */
+function aboutMe (object){
+  // instantiating the elements
+  console.log(object)
+  const card = document.createElement('div');
+  const image = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const username = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const a = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+  // setting class names, attributes and text
+  image.src = object.avatar_url;
+  name.textContent = object.name;
+  username.textContent = object.login;
+  location.textContent = `Location: ${object.location}`;
+  profile.textContent = `Profile:`;
+  a.href = object.url;
+  a.textContent = object.url;
+  followers.textContent = `Folowers: ${object.followers}`;
+  following.textContent = `Following: ${object.following}`;
+  bio.textContent = `Bio: ${object.bio}`;
 
+
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username');
+  // creating the hierarchy
+  card.appendChild(image)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(name)
+  cardInfo.appendChild(username)
+  cardInfo.appendChild(location)
+  cardInfo.appendChild(profile)
+  profile.appendChild(a)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
+  cardInfo.appendChild(bio)
+
+  
+
+
+  console.log(name)
+  console.log(card)
+  return card
+}
 /*
   List of LS Instructors Github username's:
     tetondan
